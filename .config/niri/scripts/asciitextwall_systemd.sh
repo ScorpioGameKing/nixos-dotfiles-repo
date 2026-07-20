b97fd53c3979b17af11346a233c28988c141c1c1
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-20 12:05:14 -0400

    niri updates

--- .config/niri/scripts/asciitextwall_systemd.sh
+++
@@ -1,8 +0,0 @@
-# ---------------------------------------
-# The following is am example automation
-# script to for creating and setting the 
-# generated image to a waypaper wallpaper
-# - Scorpio
-# ---------------------------------------
-/home/scorpio/Programs/AsciiTextWall/AsciiTextWall -t "Just Another $(date +%A)" -f "random" -s 30 -w 100 -c "#ebdbb2" -b "#282828" -n 'day_of_week.png' -o '/home/scorpio/Pictures/Backgrounds/Static/Asciiwall/' -mf '/usr/share/fonts/TTF/AgaveNerdFontMono-Regular.ttf'
-waypaper --wallpaper '/home/scorpio/Pictures/Backgrounds/Static/Asciiwall/day_of_week.png'
